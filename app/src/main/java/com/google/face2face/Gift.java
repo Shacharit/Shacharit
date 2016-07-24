f91e1295f48e155adbaa736baf18671849c4fdd1
nirdovrat <dovratnir@gmail.com>
2016-07-24 18:11:41 +0300

    Get gifts notification, choose a gift, and upload choice to db

---
+++ app/src/main/java/com/google/face2face/Gift.java
@@ -0,0 +1,19 @@
+package com.google.face2face;
+
+/**
+ * Created by ndovrat on 7/24/16.
+ */
+public class Gift {
+    public String sender;
+    public String recipient;
+    public String name;
+    public String event;
+    public String sent;
+    public String date;
+
+    public Gift() {
+        this.sent = "false";
+
+    }
+
+}
