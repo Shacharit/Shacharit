3d40ebc5b5fd265c9602e4cd76ce8831e7b5a868
dafnama <dafnama@gmail.com>
2016-07-25 15:37:19 +0300

    Add TV shows

--- app/src/main/java/com/google/face2face/Constants.java
+++ app/src/main/java/com/google/face2face/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String GENDER_MALE = "male";
     public static final String GENDER_OTHER = "other";
     public static final String DISPLAY_NAME = "display_name";
+    public static final String IMAGE_URL = "image_url";
 }
