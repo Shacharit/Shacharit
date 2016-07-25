d55c92aea492c32616ca66a3a0e0ecc4b667f9ff
nerya <nerya.or@gmail.com>
2016-07-25 12:15:48 +0300

    Rough sketch of a tabbed layout for the profile page.

--- app/src/main/java/com/google/face2face/ProfileFragment.java
+++ app/src/main/java/com/google/face2face/ProfileFragment.java
@@ -4,19 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentTabHost mTabHost;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -34,8 +28,6 @@ public class ProfileFragment extends Fragment {
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -43,17 +35,27 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mTabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.realtabcontent);
+
+        // TODO
+        mTabHost.addTab(mTabHost.newTabSpec("fragmentb").setIndicator("Fragment B"),
+                OtherDefinitionFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("fragmentc").setIndicator("Fragment C"),
+                OtherDefinitionFragment.class, null);
+/*        mTabHost.addTab(mTabHost.newTabSpec("fragmentd").setIndicator("Fragment D"),
+                FragmentD.class, null);
+*/
+
+        return rootView;
     }
 
     @Override
